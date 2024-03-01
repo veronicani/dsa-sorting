21a775127b9dfe36b69fcee66259c6694a32a353
Veronica Ni <vni1294@gmail.com>
2024-03-01 15:37:38 -0500

    Finished selectionSort and peopleSort

--- sorting/people-sort/peopleSort.ts
+++ sorting/people-sort/peopleSort.ts
@@ -3,7 +3,15 @@ type tPerson = {fname: string, lname: string};
 /** Sort array of persons in place (lname > fname). Returns array. */
 
 function peopleSort(arr: tPerson[]): tPerson[] {
-  return [];
+  return arr.sort((a, b) => {
+    if (a.lname > b.lname) return 1;
+    else if (a.lname < b.lname) return -1;
+    else {
+      if (a.fname > b.fname) return 1;
+      else if (a.fname < b.fname) return -1;
+      else return 0;
+    }
+  });
 }
 
 export { peopleSort };
--- sorting/selection-sort/selectionSort.ts
+++ sorting/selection-sort/selectionSort.ts
@@ -1,7 +1,21 @@
 /** Sort in-place and return sorted array. */
 
 function selectionSort(nums: number[]): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    let lowest = nums[i];
+    let lowestIdx = i;
+    let idxToSwap = i;
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[j] < lowest) {
+          lowest = nums[j];
+          idxToSwap = j;
+        }
+      }
+    nums[idxToSwap] = nums[lowestIdx];
+    nums[lowestIdx] = lowest;
+  }
   return nums;
 }
 
 export { selectionSort };
+
